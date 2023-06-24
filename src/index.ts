ca3d88615d7de8613754afbb3357429dc3c38951
crysosancher <vibhupandey.vp@gmail.com>
2023-06-24 20:50:50 +0530

    all set

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,10 @@
-// import puppeteer from "puppeteer";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// function delay(time: number) {
-//   return new Promise(function (resolve) {
-//     setTimeout(resolve, time);
-//   });
-// }
-
-// puppeteer
-//   .launch({
-//     headless: false,
-//   })
-//   .then(async (browser) => {
-//     const page = await browser.newPage();
-//     console.log(process.env.website_Url);
-//     let url = process.env.websiteUrl || "https://www.amazon.in/";
-//     await page.goto(url);
-
-//     // Click on login
-//     await page.click("#nav-link-accountList");
-//     await delay(2000);
-
-//     // Select the box
-//     let email = process.env.amazonEmail || "";
-
-//     // Write email Id
-//     await page.type("#ap_email", email);
-
-//     // Click on continue
-//     await page.click("#continue");
-//     await delay(2000);
-
-//     // Write password
-//     let password = process.env.amazonPassword || "";
-//     await page.type("#ap_password", password);
-
-//     // Click on login
-//     await page.click("#signInSubmit");
-//     await delay(3000);
-
-//     // Click on search box
-//     await page.click("#twotabsearchtextbox");
-
-//     // Write product name
-//     await page.type("#twotabsearchtextbox", "iphone 11");
-
-//     // Click on search button
-//     await page.click("#nav-search-submit-button");
-//     await delay(2000);
-
-//     // Wait for page to load
-//     await page.waitForSelector("body");
-    
-
-//     const products = await page.evaluate(() => {
-//       let productInfo = [];
-//       const searchResults:any = document.querySelectorAll(".sg-col-20-of-24.s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.s-widget-spacing-small.sg-col-12-of-16");
-
-//       for (const searchResult of searchResults) {
-//         try {
-//           let title = searchResult.querySelector("h2 a span").textContent.trim();
-//           let price = searchResult.querySelector("span.a-price span").textContent.trim();
-//           let rating = searchResult.querySelector("span.a-icon-alt").textContent.trim();
-//           let reviewCount = searchResult.querySelector("span.a-size-base").textContent.trim();
-
-//           productInfo.push({
-//             title,
-//             price,
-//             rating,
-//             reviewCount,
-//           });
-//         } catch (e) {
-//           console.log(e);
-//         }
-//       }
-
-//       return productInfo;
-//     });
-
-//     console.log(products);
-//   });
-import puppeteer from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-function delay(time: number) {
+function delay(time: number): Promise<void> {
   return new Promise(function (resolve) {
     setTimeout(resolve, time);
   });
@@ -97,10 +14,10 @@ puppeteer
   .launch({
     headless: false,
   })
-  .then(async (browser) => {
-    const page = await browser.newPage();
-    console.log(process.env.website_Url);
-    let url = process.env.websiteUrl || "https://www.amazon.in/";
+  .then(async (browser: Browser) => {
+    const page: Page = await browser.newPage();
+    console.log(process.env.websiteUrl);
+    let url: string = process.env.websiteUrl || "https://www.amazon.in/";
     await page.goto(url);
 
     // Click on login
@@ -108,7 +25,7 @@ puppeteer
     await delay(2000);
 
     // Select the box
-    let email = process.env.amazonEmail || "";
+    let email: string = process.env.amazonEmail || "";
 
     // Write email Id
     await page.type("#ap_email", email);
@@ -118,7 +35,7 @@ puppeteer
     await delay(2000);
 
     // Write password
-    let password = process.env.amazonPassword || "";
+    let password: string = process.env.amazonPassword || "";
     await page.type("#ap_password", password);
 
     // Click on login
@@ -129,7 +46,7 @@ puppeteer
     await page.click("#twotabsearchtextbox");
 
     // Write product name
-    await page.type("#twotabsearchtextbox", process.env.productName || "");
+    await page.type("#twotabsearchtextbox", "toothpaste");
 
     // Click on search button
     await page.click("#nav-search-submit-button");
@@ -138,19 +55,38 @@ puppeteer
     // Wait for page to load
     await page.waitForSelector("body");
 
-    const products = await page.evaluate(() => {
-      let productInfo = [];
-      const searchResults:any = document.querySelectorAll(
-        ".sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(n+7)"
+    const products: any[] = await page.evaluate(async () => {
+      let productInfo: any[] = [];
+      const searchResults: any = document.querySelectorAll(
+        ".s-search-results .s-result-item"
       );
 
       for (let i = 0; i < Math.min(10, searchResults.length); i++) {
         try {
+					setTimeout(() =>(2000));
           let searchResult = searchResults[i];
-          let title = searchResult.querySelector("h2 a span").textContent.trim();
-          let price = searchResult.querySelector("span.a-price span").textContent.trim();
-          let rating = searchResult.querySelector("span.a-icon-alt").textContent.trim();
-          let reviewCount = searchResult.querySelector("span.a-size-base").textContent.trim();
+          let title = searchResult
+            .querySelector("h2 a span")
+            .textContent.trim();
+          let price = searchResult
+            .querySelector(".a-price-whole")
+            .textContent.trim();
+          let rating = searchResult
+            .querySelector(".a-icon-star-small .a-icon-alt")
+            .textContent.trim();
+          let reviewCount = searchResult
+            .querySelector(".a-size-small .a-link-normal")
+            .textContent.trim();
+          if (i > 1 && i < 6) {
+						console.log(i,"=>",document
+						.querySelector(`#a-autoid-${i}-announce`));
+            const k:any=document
+              .querySelector(`#a-autoid-${i}-announce`)
+							console.log(k);
+							k.click();
+
+              
+          }
 
           productInfo.push({
             title,
@@ -168,64 +104,59 @@ puppeteer
 
     console.log("Top 10 search results:");
     console.log(products);
-    //write to a file
-    const fs = require("fs");
-    fs.writeFile("public/products.json", JSON.stringify(products), function (err: any) {
-      if (err) throw err;
-      console.log("Saved!");
+
+    //Go to cart
+		await delay(2000);
+    await page.goto("https://www.amazon.in/gp/cart/view.html");
+    
+await page.waitForSelector(".sc-list-item-content");
+    // Extract cart information
+    const cartItems: any[] = await page.evaluate(() => {
+      let cartInfo: any[] = [];
+			const finalPrice:any=document.querySelector("#sc-subtotal-amount-activecart > span")
+			const finalPriceText=finalPrice.textContent.trim();
+      const cartItems = document.querySelectorAll(".sc-list-item-content");
+			console.log(cartItems);
+
+      for (let i = 0; i < cartItems.length; i++) {
+        try {
+          let cartItem:any = cartItems[i];
+          let title = cartItem.querySelector(".a-truncate-cut").textContent.trim();
+          let price = cartItem.querySelector(".sc-item-price-block p span").textContent.trim();
+          let quantity = cartItem.querySelector('.sc-action-quantity').getAttribute("data-old-value")
+
+          cartInfo.push({
+            title,
+            price,
+            quantity,
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+			cartInfo.push({
+				finalPriceText
+			})
+
+      return cartInfo;
     });
 
-    // Add top 4 products to cart
-    for (let i = 0; i < Math.min(4, products.length); i++) {
-      const product = products[i];
+    console.log("Cart items:");
+    console.log(cartItems);
 
-      // Click on the product
-      await page.click(`.sg-col-20-of-24:nth-child(${i + 1}) .a-link-normal.a-text-normal`);
+    // Write products and cart items to files
+    fs.writeFile("public/products.json", JSON.stringify(products), function (err) {
+      if (err) throw err;
+      console.log("Products saved!");
+    });
 
-      // Wait for page to load
-      await delay(2000);
+    fs.writeFile("public/cartItems.json", JSON.stringify(cartItems), function (err) {
+      if (err) throw err;
+      console.log("Cart items saved!");
+    });
 
-      // Add to cart
-      // await page.click("#add-to-cart-button");
-      // await delay(2000);
-
-      // // Go back to search results
-      // await page.goBack();
-      // await delay(2000);
-    }
-
-    // Go to cart
-    // await page.goto("https://www.amazon.in/gp/cart/view.html");
-    // await delay(2000);
-
-    // // Extract information from the cart
-    // const cartItems = await page.evaluate(() => {
-    //   let cartInfo = [];
-    //   const cartItems:any = document.querySelectorAll(".sc-list-item-content");
-
-    //   for (const cartItem of cartItems) {
-    //     try {
-    //       let title = cartItem.querySelector(".sc-product-title a").textContent.trim();
-    //       let price = cartItem.querySelector(".sc-price span").textContent.trim();
-    //       let quantity = cartItem.querySelector(".sc-quantity-textfield input").value.trim();
-    //       let total = cartItem.querySelector(".sc-product-price").textContent.trim();
-
-    //       cartInfo.push({
-    //         title,
-    //         price,
-    //         quantity,
-    //         total,
-    //       });
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //   }
-
-    //   return cartInfo;
-    // });
-
-    // console.log("Cart items:");
-    // console.log(cartItems);
-
-    // await browser.close();
+    //await browser.close();
+  })
+  .catch((err) => {
+    console.log(err);
   });
